937c9d04e19b3535cf9604b2eaa2f52f0f975ae2
Ethan <ethancheung2013@gmail.com>
2014-07-04 22:13:17 -0700

    scraper update with unicode handling

--- scraper.py
+++ scraper.py
@@ -2,11 +2,17 @@ import bs4 as bs4
 import requests
 import urlparse
 import time
+import sqlite3 as lite
+from datetime import datetime as dt
+
 
 url = 'http://finance.yahoo.com/news/provider-marketwatch/'
 url2 = "http://finance.yahoo.com/_xhr/top-story-temp/"
 
 form_data = "list_source=collection&apply_filter=0&filters=[]&content_id=6647ea16-dcee-3757-80a0-49b1b43f2f47&categories=%5B%5D&selected_tab=0&latest_on=0&s=1183300001&sec=MediaTopStoryTemp&story_start=51&storycount=50&popup_switch=1&provider_switch=1&timestamp_switch=1&max_title_length=100&more_inline=1&base_start=1&cache_ttl=TTL_LEVEL_10"
+
+conn = lite.connect('data.sqlite/financenews')
+cur = conn.cursor()
 
 s = requests.Session()
 s.head(url)
@@ -16,19 +22,31 @@ s.head(url)
 sq = urlparse.parse_qsl(form_data)
 params = dict(sq)
 
+# with conn:
+
 for i in range(0, 101, 50):
     params['story_start'] = i
     res = s.post(url2, data=params, headers={'Referer': url})
 
     soup = bs4.BeautifulSoup(res.content)
 
-    soup.li.cite.text.encode('utf-8').split('-')[-1].strip()
+    print ">>>>>>>>>>>>>>>",soup.li
+    try:
+        soup.li.cite.text.encode('utf-8').split('-')[-1].strip()
+    except:
+        print "PASSING"
+        pass
 
     for article in soup.find_all('li'):
         print "Title:"
         print article.a.text.encode('utf-8')
-        url = article.a.get('href')
-        date = article.cite.text.split('-')[-1].strip()
+        title = article.a.text.encode('utf-8').replace("'",'').replace('"','')
+        url = article.a.get('href').encode('utf-8')
+        try:
+            date = dt.strptime( article.cite.text.encode('utf-8').split('-')[-1].strip() , '%b %d %Y %I:%M%p')
+        except:
+            date = dt.now()
+        content = ''
         print '\n', url, '\n', date
         try:
             more_articles = requests.get(url)
@@ -50,7 +68,10 @@ for i in range(0, 101, 50):
                 for eMainStory in mainStory:
                     innerP = eMainStory.findChildren('p', { "class" : "" })
                     for u in range(len(innerP)):
-                        print ' '.join(innerP[u].text.encode('utf-8').split())
+                        # print ' '.join(innerP[u].text.encode('utf-8').split())
+                        # content += ' '.join(innerP[u].text.encode('utf-8').split())
+                        content += ' '.join(innerP[u].text.encode('utf-8').replace('"','').replace("'",'').split())
+                        # print content
             else: # this handles links that have videos/audio
                 innerP = innerSoup.find_all("div", { "class" : "currentVideoInfo"})
                 if len(innerSoup.find_all("div", { "class" : "currentVideoInfo"})) == 0:
@@ -63,8 +84,24 @@ for i in range(0, 101, 50):
                     for i in range(len(innerP)):
                         eMainStory = innerP[i].findChildren('p')
                         for u in range(len(eMainStory)):
-                            # print ' '.join(eMainStory[u].encode_contents().split())
-                            print ' '.join(eMainStory[u].text.encode('utf-8').split())
+                            
+                            # print ' '.join(eMainStory[u].text.encode('utf-8').split())
+                            # content += ' '.join(eMainStory[u].text.encode('utf-8').split())
+                            content += ' '.join(eMainStory[u].text.encode('utf-8').replace('"','').replace("'",'').split())
+                            # print content
+        # print content
+        print title
+        print url
+        print date
+        with conn:
+            # whatis = 'INSERT INTO data (title, content, url, date) VALUES( "' + title + '","'+content+'","'+url+'","'+str(date)+'")'
+            whatis = "INSERT INTO data (title, content, url, date) VALUES( '" + title + "','"+content+"','"+url+"','"+str(date)+"')"
+            print "HHHHHH ", whatis
+            # cur.execute("INSERT INTO data (title, content, url, date) VALUES(" + title + "," + content + " ," + url + ", "+ date + ")")
+            cur.execute(whatis)
+            # cur.execute("INSERT INTO data (title, content, url, date) VALUES('title' ,'content','url','date')")
 
         print "\n"
     time.sleep(0.5) 
+if conn:
+    conn.close()
